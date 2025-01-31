6b41b5cb775e42061420c68569d63a731282bdbd
griffinslt <slt.griffin3105@gmail.com>
2025-01-31 14:42:08 +0000

    Write some theory data

---
+++ src/composables/theoryData.js
@@ -0,0 +1,15 @@
+const steps =
+    [
+        {
+            title: "Introduction",
+            content: ["Point 1", "Point 2", "Point 3"]
+        },
+        {
+            title: "Calculate Key",
+            content: ["We can calculate the key by finding the number of sharps or flats and seeing if any accidentals are used to signify whether they are major or minor."]
+        }
+    ]
+
+
+
+export default { steps }
