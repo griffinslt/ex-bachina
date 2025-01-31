9c1db6527443360dacaf17a22a1169fc7b9ab7e9
griffinslt <slt.griffin3105@gmail.com>
2025-01-31 14:41:29 +0000

    Import v-networkgraph

--- src/main.js
+++ src/main.js
@@ -5,11 +5,14 @@ import "/src/assets/main.scss"
 
 
 import { createApp } from 'vue'
+import VNetworkGraph from "v-network-graph"
+import "v-network-graph/lib/style.css"
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
+app.use(VNetworkGraph)
 app.use(router)
 
 app.mount('#app')
