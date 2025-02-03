181205f49cf0942e65178f70134d4c0e9edbcd42
griffinslt <slt.griffin3105@gmail.com>
2025-02-03 10:58:28 +0000

    Format navbar properly

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,34 @@
   <header>
     <div class="wrapper">
       <nav>
-
+        <div class="row text-end">
+    <p>
+      <RouterLink to="/" class="btn btn-dark-outline btn-lg shadow-none">Walkthough</RouterLink> | 
+      <RouterLink to="/steps" class="btn btn-dark-outline btn-lg shadow-none">Steps</RouterLink> | 
+      <button class="btn btn-dark-outline btn-lg border-none">Interactive Stave</button> | 
+      <button class="btn btn-dark-outline btn-lg shadow-none">Generate</button></p>
+    <br><br>
+  </div>
       </nav>
     </div>
   </header>
 
   <RouterView />
 </template>
+
+<script>
+
+import { RouterLink, RouterView } from 'vue-router'
+export default {
+  setup(){
+
+
+
+
+return {RouterLink, RouterView}
+  }
+}
+</script>
+
+
 
