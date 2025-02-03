bd78144b2b950c39767eeb1b91ea8ea627c83eb7
griffinslt <slt.griffin3105@gmail.com>
2025-02-03 20:51:31 +0000

    Add other titles for the steps

--- src/composables/theoryData.js
+++ src/composables/theoryData.js
@@ -6,8 +6,37 @@ const steps =
         },
         {
             title: "Calculate Key",
-            content: ["We can calculate the key by finding the number of sharps or flats and seeing if any accidentals are used to signify whether they are major or minor."]
-        }
+            content: [
+                "We can calculate the key by finding the number of sharps or flats and seeing if any accidentals are used to signify whether they are major or minor.",
+                "If the key is major, 0 sharps means the key is C and if it is minor it is A.",
+                "If the key is major, sharps numbering from 1-6 means the key would be G, D, A, E, B or F#",
+                "If the key is major, flat numbering from 1-6 means the key would be F, Bb, Eb, Ab, Eb, Ab, Db or Gb",
+                "If the key is minor, sharps numbering from 1-6 means the key would be E, B, F#, C#, G# or D#",
+                "If the key is minor, flat numbering from 1-6 means the key would be D, G, C, F, Bb or Eb",
+
+            ]
+        },
+        {
+            title: "Find Cadence Locations",
+            content: []
+        },
+        {
+            title: "Choose Cadence Chords",
+            content: []
+        },
+        {
+            title: "Selecting other chords",
+            content: []
+        },
+        {
+            title: "Select bass notes",
+            content: []
+        },
+        {
+            title: "Select alto and tenor notes",
+            content: []
+        },
+
     ]
 
 
