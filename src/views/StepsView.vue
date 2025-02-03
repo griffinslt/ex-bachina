26b880ccb0b93a0cb30855fc8b17b45468fe2260
griffinslt <slt.griffin3105@gmail.com>
2025-02-03 11:52:46 +0000

    Rename steps and add flashcard

---
+++ src/views/StepsView.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Steps</h1>
+
+    <p>Click a Flashcard to reveal the content </p>
+    <div class="row">
+        <div  v-for="step in steps" :key=step.title class="col">
+            <FlashCard :step="step"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import theoryData from '@/composables/theoryData';
+import FlashCard from '@/components/FlashCard.vue';
+
+
+export default {
+    setup() {
+        const steps = theoryData.steps;
+        return { steps };
+    },
+    components: { FlashCard }
+}
+
+</script>
