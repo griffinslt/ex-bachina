c7fb8e4d75b7a700bf171fdf42682fe880b697a2
griffinslt <slt.griffin3105@gmail.com>
2025-02-19 09:50:34 +0000

    Write bassline

--- src/classes/ABCChorale.js
+++ src/classes/ABCChorale.js
@@ -1,3 +1,4 @@
+import jsHelpers from "@/jsHelpers";
 import { Chord, Key } from "tonal";
 
 export default class ABCChorale {
@@ -5,71 +6,101 @@ export default class ABCChorale {
         this.abcString = abcString;
         this.noteList = chorale.getNoteList();
         this.chorale = chorale;
-        this.run();        
+        this.linebreaks = null;
+        this.run();
     }
 
-    run(){
+    run() {
         this.addOtherParts()
-        // const voice1Bars = this.findVoiceLine(1).split(" | ");
+        const voice1Bars = this.findVoiceLine(1).split(" | ");
+        this.linebreaks = jsHelpers.indexOfItemInElement(voice1Bars, "\n");
         // console.log(voice1Bars)
-        // var voice4Bars = voice1Bars;
         this.writeBassLine();
+        this.writeAltoAndTenorParts();
 
-        
+
     }
 
-    getString(){
+    getLineBreaks() {
+    }
+
+    getString() {
         console.log(this.abcString)
         return this.abcString;
     }
 
-    addOtherParts(){
+    addOtherParts() {
         const stringToSearch = "V:1 treble ";
         const startingIndex = this.abcString.indexOf(stringToSearch);
-        var startSubstring = this.abcString.substring(0,startingIndex + stringToSearch.length + 1)
-        const endSubstring = this.abcString.substring(startingIndex + stringToSearch.length + 1,this.abcString.length)
+        var startSubstring = this.abcString.substring(0, startingIndex + stringToSearch.length + 1)
+        const endSubstring = this.abcString.substring(startingIndex + stringToSearch.length + 1, this.abcString.length)
 
         // startSubstring += "V2: treble\nV3: bass\nV4:bass\n";
         startSubstring += "V:4 bass\n";
-        
+
         this.abcString = startSubstring + endSubstring;
 
-        
+
 
     }
 
-    findVoiceLine(num){
+    findVoiceLine(num) {
         // console.log(this.abcString)
-        const startingIndex = this.abcString.indexOf("V:"+ num +"\n") + 4;
+        const startingIndex = this.abcString.indexOf("V:" + num + "\n") + 4;
         const stringPostStartingIndex = this.abcString.substring(startingIndex, this.abcString.length);
         return stringPostStartingIndex.substring(0, stringPostStartingIndex.indexOf('|]'));
     }
 
-    writeBassLine(){
+
+    writeAltoAndTenorParts(){
+        var altoLineString = "V:2\n";
+        var tenorLineString = "V:3\n";
+
+
+    }
+
+    writeBassLine() {
         var bassLineString = "V:4\n";
         var currentBar = 0;
-        const key = this.getKey();
-        for(const note of this.noteList){
+        var numOfBars = 0;
+        for (const note of this.noteList) {
             if (currentBar < note.barNumber) {
                 bassLineString += " |";
                 currentBar = note.barNumber;
+                numOfBars++;
+                if (numOfBars == this.linebreaks+1) {
+                    bassLineString += "\n";
+                }
             }
             if (note.pitch != null) {
-                if (typeof note.chord == null) {
+                const bassNote = this.getBassNote(note.chord, this.getKey())
+                if (typeof bassNote == "undefined") {
                     console.log(note)
                 }
-                const bassNote = this.getBassNote(note.chord, this.getKey())
+
                 bassLineString += " " + bassNote;
             } else {
                 bassLineString += " z"
             }
+            if (note.type == "half") {
+                var numToAdd = 2;
+                if (note.dot) {
+                    numToAdd ++;
+                }
+                bassLineString += numToAdd;
+            }
+            if (note.nextNote == null) {
+                bassLineString += "|]";
+            }
+
+            
 
         }
 
         this.abcString += bassLineString;
     }
 
-    getBassNote(chord, key){
+    getBassNote(chord, key) {
         if (typeof chord == "undefined") {
             return; // sort this out
         }
@@ -77,12 +108,12 @@ export default class ABCChorale {
         const triad = this.chorale.getTriadFromNumeral(formattedChord.numeral, key);
         switch (formattedChord.inversion) {
             case "a":
-                return triad[0]
+                return triad[0] + ",,";
             case "b":
-                return triad[1]
+                return triad[1] + ",,";
             case "c":
-                return triad[2]
-        
+                return triad[2] + ",,";
+
             default:
                 throw Error("Invalid inversion");
         }
@@ -92,17 +123,17 @@ export default class ABCChorale {
         // return chordNotes;
     }
 
-    getKey(){
+    getKey() {
         const stringToSearch = "K:";
-        const startingIndex = this.abcString.indexOf(stringToSearch)+2;
+        const startingIndex = this.abcString.indexOf(stringToSearch) + 2;
         const stringAfterStaringIndex = this.abcString.substring(startingIndex, this.abcString.length);
         const endingIndex = stringAfterStaringIndex.indexOf("\n");
         const key = stringAfterStaringIndex.substring(0, endingIndex);
 
-        if (key.includes("m")){
+        if (key.includes("m")) {
             const tonic = key.substring(0, key.indexOf("m"));
             return Key.minorKey(tonic);
-        } else{
+        } else {
             return Key.majorKey(key);
         }
     }
