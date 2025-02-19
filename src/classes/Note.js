d804f612b63154eb5933ec463f45ac5184c8cdfe
griffinslt <slt.griffin3105@gmail.com>
2025-02-19 09:49:54 +0000

    Add attribute to MyNote class for storing whether a note is dotted

--- src/classes/Note.js
+++ src/classes/Note.js
@@ -1,11 +1,12 @@
 export default class MyNote {
-    constructor(previousNote=null, nextNote=null, pitch=null, type, barNumber) {
+    constructor(previousNote=null, nextNote=null, pitch=null, type, barNumber, dot) {
         this.previousNote = previousNote;
         this.nextNote = nextNote;
         this.pitch = pitch;
         this.type = type;
         this.barNumber = barNumber;
         this.chord = null
+        this.dot = dot;
     }
 
     setNextNote(note){
