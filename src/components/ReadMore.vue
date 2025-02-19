15c4183616413109600522e4ce52a141dc3c2ce1
griffinslt <slt.griffin3105@gmail.com>
2025-02-19 16:52:33 +0000

    Create read more component

---
+++ src/components/ReadMore.vue
@@ -0,0 +1,31 @@
+<template>
+
+    <ul v-for="(point, index) in steps" :key="index">
+        <li v-if="showContent || index > 3">{{ point }}</li>
+    </ul>
+    <span class="btn btn-link" @click="toggleContent" v-if="!showContent && steps.length > 2">Read More</span>
+    <span class="btn btn-link" @click="toggleContent" v-if="showContent && steps.length > 2">Read Less</span>
+</template>
+
+
+<script>
+import { ref } from 'vue';
+
+
+
+export default {
+    props: ['steps'],
+    setup(props) {
+        const steps = ref(props.steps);
+        const showContent = ref(false);
+
+        const toggleContent = () => {
+            showContent.value = !showContent.value
+        }
+
+        return { steps, showContent, toggleContent }
+    }
+
+}
+
+</script>
