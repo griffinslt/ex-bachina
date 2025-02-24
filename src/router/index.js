a107d9845c49a22e63eb471bace8249495c0ee71
griffinslt <slt.griffin3105@gmail.com>
2025-02-24 11:02:20 +0000

    Update the name of the walkthrough view

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import WalkthroughView from '../views/WalkthroughView.vue'
 import Steps from '../views/StepsView.vue'
 
 const router = createRouter({
@@ -7,8 +7,8 @@ const router = createRouter({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'walkthrough',
+      component: WalkthroughView
     },
     {
       path: '/steps',
