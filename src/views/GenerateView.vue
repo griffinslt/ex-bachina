8170fe7ac3d929318e9f6f4d755ba8924795bc03
griffinslt <slt.griffin3105@gmail.com>
2025-02-25 17:31:39 +0000

    Make view for the generate feature

---
+++ src/views/GenerateView.vue
@@ -0,0 +1,12 @@
+<template>
+<h1>Generate</h1>
+<p>This page is simply to select a chorale and export it as MusicXML to be opened into some other software.</p>
+</template>
+
+<script>
+export default {
+    setup(){
+        
+    }
+}
+</script>
