f8e005730b5b42a0134c5b0fbf3ed510c06d49a8
griffinslt <slt.griffin3105@gmail.com>
2025-02-26 20:05:00 +0000

    Create modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,27 @@
+<template>
+    <div style="top: 0;position: fixed;background: rgba(0, 0, 0, 0.5);width: 100%;height: 100%;" class=" d-flex justify-content-center"  @click="$emit('close')">
+        <div class="card my-5 p-2 text-center" style="min-width: 500px;"  @click="$emit('close')">
+            <slot>
+
+            </slot>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    setup(){
+        
+    }
+    
+    
+}
+</script>
+
+
+
+
+<style>
+
+</style>
