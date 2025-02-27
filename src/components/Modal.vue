7aa1ba49e5665b43c3f38ffaa0885ff4cc3b6d36
griffinslt <slt.griffin3105@gmail.com>
2025-02-27 08:44:39 +0000

    Change modal height

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="top: 0;position: fixed;background: rgba(0, 0, 0, 0.5);width: 100%;height: 100%;" class=" d-flex justify-content-center"  @click="$emit('close')">
-        <div class="card my-5 p-2 text-center" style="min-width: 500px;"  @click="$emit('close')">
+        <div class="card my-5 p-2 text-center" style="min-width: 500px; height: 60%;"  @click="$emit('close')">
             <slot>
 
             </slot>
