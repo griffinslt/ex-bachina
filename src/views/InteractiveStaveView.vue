74e66031f537751d7278111ccd63cd170cce9af9
griffinslt <slt.griffin3105@gmail.com>
2025-03-03 15:19:37 +0000

    Start adding comntent to interactive view

--- src/views/InteractiveStaveView.vue
+++ src/views/InteractiveStaveView.vue
@@ -1,11 +1,31 @@
 <template>
-<h1>Interactive Stave</h1>
+    <div class="row text-center">
+        <div class="col">
+            <h1>Interactive Stave</h1>
+            <div id="target"></div>
+            <div id="audio" class=""></div>
+        </div>
+        <div class="col">
+            <br>
+            <FlowDiagram :currentStep="currentStep" :key="componentKey" />
+        </div>
+    </div>
 </template>
 
 <script>
-export default {
-    setup(){
 
-    }
+import FlowDiagram from '@/components/FlowDiagram.vue';
+import { ref } from 'vue';
+
+export default {
+    setup() {
+        const componentKey = ref(1);
+        const currentStep = ref(0);
+        var chorale = null;
+        var abcChorale = null;
+
+        return { componentKey, currentStep}
+    },
+    components: { FlowDiagram }
 }
 </script>
