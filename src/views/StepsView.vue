311901992232e4728641f6ef73b241747c354a82
griffinslt <slt.griffin3105@gmail.com>
2025-04-15 16:59:29 +0100

    Add functinoality of adding flashcard

--- src/views/StepsView.vue
+++ src/views/StepsView.vue
@@ -1,6 +1,10 @@
 <template>
     <h1>Steps</h1>
-    <p>There is a flashcard for each step of the harmonising process. Click on a flashcard to reveal the content </p>
+    <div class="row">
+        <div class="col-md-8"><p>There is a flashcard for each step of the harmonising process. Click on a flashcard to reveal the content </p></div>
+        <div class="col text-end"><button @click="toggleAddNewFlashCard" class="btn btn-primary text-white">Add new flashcard</button></div>
+    </div>
+    
     <hr>
     <div class="row">
         <div class="col">
@@ -10,25 +14,52 @@
                 </div>
             </div>
         </div>
-        <!-- <div class="col">
-            <FlowDiagram />
-        </div> -->
     </div>
+
+    <div v-if="showAddNewFlashCardModal">
+    <Modal @close="toggleAddNewFlashCard">
+      <h5>Add a new flashcard</h5>
+      <form @submit.prevent="handleSubmit" class="p-2">
+        <input required type="text" v-model="term" placeholder="Term" class="form-control m-2">
+        <textarea required class="form-control m-2" v-model="definition" rows="7" placeholder="Definition"></textarea>
+
+        <button class="btn btn-primary text-white">Submit</button>
+      </form>
+    </Modal>
+</div>
 </template>
 
 <script>
 import theoryData from '@/composables/theoryData';
 import FlashCard from '@/components/FlashCard.vue';
 import FlowDiagram from '@/components/FlowDiagram.vue';
+import { ref } from 'vue';
+import Modal from '@/components/Modal.vue';
+
 
 
 export default {
     setup() {
-        const steps = theoryData.steps.slice(1).slice(0, -1);
+        const steps = ref(theoryData.steps.slice(1).slice(0, -1));
+        const showAddNewFlashCardModal = ref(false);
+        const term = ref("");
+        const definition = ref("");
         
-        return { steps };
+        const toggleAddNewFlashCard = () => {
+            showAddNewFlashCardModal.value = !showAddNewFlashCardModal.value;
+        }
+
+        const handleSubmit = () => {
+            steps.value.push({
+                title: term.value,
+                content: [definition.value]
+            });
+            toggleAddNewFlashCard();
+        }
+        
+        return { steps, showAddNewFlashCardModal, term, definition, toggleAddNewFlashCard, handleSubmit};
     },
-    components: { FlashCard, FlowDiagram }
+    components: { FlashCard, FlowDiagram, Modal }
 }
 
 </script>
