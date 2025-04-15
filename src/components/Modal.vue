a75078189baefc9f9f94d2010bae206e7e07ce30
griffinslt <slt.griffin3105@gmail.com>
2025-04-15 16:59:05 +0100

    Fix modal - it was dissapearing even when it was the modal being clicked

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,6 @@
 <template>
     <div style="background: rgba(0, 0, 0, 0.5);" class="d-flex justify-content-center full-width"  @click="$emit('close')">
-        <div class="card my-5 p-2 text-center" style="min-width: 500px; height: 50%;"  @click="$emit('close')">
+        <div class="card my-5 p-2 text-center" style="min-width: 500px; height: 50%;"  @click.stop="">
             <slot>
 
             </slot>
@@ -34,6 +34,6 @@ export default {
     top: 50%;
     bottom: 50%;
     margin-top: -30vw;
-    margin-bottom: -50vw;
+    margin-bottom: 20vw;
 }
 </style>
