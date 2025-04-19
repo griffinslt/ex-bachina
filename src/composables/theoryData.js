47affa5d3979b5e8f17b94fe480dc5ea431a08aa
griffinslt <slt.griffin3105@gmail.com>
2025-04-19 16:33:47 +0100

    Fix typos

--- src/composables/theoryData.js
+++ src/composables/theoryData.js
@@ -2,10 +2,10 @@ const steps =
     [
         {
             title: "Introduction",
-            content: ["Welcome to the walkthough.",
-                "Navigate with the below buttons to learn the process of harmonising a chorale.",
+            content: ["Welcome to the walkthrough.",
+                "Navigate with the buttons above to learn the process of harmonising a chorale.",
                 "Feel free to press the above play button to hear the music play back, though that is not necessary to understand the process.",
-                "It may also be worth to glance at the flow diagram to get an overview of the whole process. This demostrates the decision making process, especially make note of the selection nodes and the loops in the diagram.",
+                "It may also be worth glancing at the flow diagram to get an overview of the whole process. This demonstrates the decision-making process, especially make note of the selection nodes and the loops in the diagram.",
             
             ]
         },
@@ -13,7 +13,7 @@ const steps =
             title: "Calculate Key",
             content: [
                 "We can calculate the key by finding the number of sharps (#) or flats (b) in the key signature and seeing if any accidentals (sharps or flats not in the key signature) are used to signify whether they are major or minor.",
-                "If the key is major, 0 sharps means the key is C and if it is minor it is A.",
+                "If the key is major, 0 sharps means the key is C, and if it is minor, it is A.",
                 "If the key is major, sharps numbering from 1-6 means the key would be G, D, A, E, B or F#",
                 "If the key is major, flat numbering from 1-6 means the key would be F, Bb, Eb, Ab, Eb, Ab, Db or Gb",
                 "If the key is minor, sharps numbering from 1-6 means the key would be E, B, F#, C#, G# or D#",
@@ -30,7 +30,7 @@ const steps =
         {
             title: "Choose Cadence Chords",
             content: [
-                "For each of the cadence locations look at the pattern in the melody line e.g. if the key is G major and the notes leading up to the cadence are B, A and G the pattern is 3-2-1",
+                "For each of the cadence locations, look at the pattern in the melody line e.g. if the key is G major and the notes leading up to the cadence are B, A and G, the pattern is 3-2-1",
                 "If the pattern is 3-2-1 the possible chord progressions are Ic-V-I or Ib-V-I.",
                 "If the pattern is 2-2-1 or 2-2-3 the possible chord progressions are ii7b-V-I or V-I.",
                 "If the pattern is 8-7-8 or 1-7-1 (the same thing) the possible chord progressions are V-I, ii7b-V-I or Ic-V-I.",
@@ -56,7 +56,7 @@ const steps =
             content: [
                 "Bass notes are easy to select they are just the bottom note from the chord selected in the previous step.",
                 "Passing notes, which are notes between chord progressions, can be added to the bass here when there are 2 steps between one bass note and another.",
-                "Auxillary notes can be added when the next note is the same as the current note. They are a step below the note and they go between the two identical notes."
+                "Auxiliary notes can be added when the next note is the same as the current note. They are a step below the note and they go between the two identical notes."
             ]
         },
         {
@@ -65,7 +65,7 @@ const steps =
                 "The alto and tenor notes are selected together.",
                 "The simplest and most important rule you need to follow here is that you want to complete the chord e.g. if you have G Major chord (GBD) and the bass part has a D and the soprano part has a G either the tenor or alto part should have a B. That leaves you free to pick the other part with what would work best.",
                 "How do you know what works best - general rules to follow are moving in step is best (so whichever notes would cause the smallest jump) and you also want to avoid parallel movement (that is when parts move in the same direction with the same steps at different pitches).",
-                "Again, do not forget to add the passing and auxillary notes for intrigure."
+                "Again, do not forget to add the passing and auxiliary notes for intrigue."
 
             ]
         },
